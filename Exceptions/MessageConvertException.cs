c74cf12c090bf0ca717ac2fede409d7a870b9d35
xklima34 <xklima34@stud.fit.vutbr.cz>
2024-04-19 16:49:47 +0200

    MessageConvertException added

---
+++ Exceptions/MessageConvertException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTA_Chat_Server.Exceptions
+{
+    public class MessageConvertException : Exception
+    {
+    }
+}
