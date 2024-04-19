4dea0cb9f3fd598dbe952c2cc2eb7ef379c067b5
xklima34 <xklima34@stud.fit.vutbr.cz>
2024-04-19 15:23:29 +0200

    Client model added

---
+++ Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOTA_Chat_Server.Models
+{
+    public class Client
+    {
+        public string Username { get; set; }
+        public string Displayname { get; set; }
+        public string? CurrentChannelName { get; set; }
+        public ClientState State { get; set; }
+
+    };
+    
+    public enum ClientState
+    {
+        NONAUTH, 
+        AUTH,
+        BYE
+    }
+}
