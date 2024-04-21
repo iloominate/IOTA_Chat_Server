91a033046b8754e75c4c8915d258d6373844033c
xklima34 <xklima34@stud.fit.vutbr.cz>
2024-04-22 01:01:44 +0200

    Udp packet processing added

--- Program.cs
+++ Program.cs
@@ -11,11 +11,11 @@ using System.Net.Http.Headers;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-﻿namespace IOTA_Chat_Server
+namespace IOTA_Chat_Server
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static ConcurrentQueue<Client> clients = new ConcurrentQueue<Client>();
         public static byte udpRetransmissionLimit = 3;
         public static UInt16 udpRetransmissionTimeout = 250;
         static async Task Main(string[] args)
@@ -75,8 +75,119 @@ using System.Runtime.InteropServices;
                         return;
                 }
             }
+
+
+            UdpClient udpListener = new UdpClient();
+            var EP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4567);
+            udpListener.Client.Bind(EP);
+           
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 4567);
+
+            
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for the first packet");
+
+                    UdpReceiveResult result = await udpListener.ReceiveAsync();
+                    Console.WriteLine("Packet has arrived");
+                    ProcessMessage(result);
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            { 
+            }
+        }
+
+        static async Task ProcessMessage(UdpReceiveResult result)
+        {
+
+            // Check message validity
+            Message? messageParsed = null;
+            try
+            {
+                messageParsed = MessageToBytesConverter.BytesToMessage(result.Buffer);
+            } catch (MessageConvertException e) // Unknown message was caught
+            {
+                // Send Bye or whatever
+                return;
+            }
+
+            // Client creation
+            IPEndPoint clientEP = result.RemoteEndPoint;
+            UdpClient? localEP = new UdpClient();
+            localEP.Client.Bind(new IPEndPoint(IPAddress.Any, 0));
+
+            Client? client = new Client(clientEP, localEP);
+            
+            client.MessagesUprocessed.Add(messageParsed);
+            var sender = Task.Run(() => SenderAsync(client));
+            client.Exit = true;
+            var listener = Task.Run(() => ListenerAsync(client));
+        }
+        public static async Task ListenerAsync(Client client)
+        {
+            Console.WriteLine("Listening on port");
+            while (client.Exit != true)
+            {
+                UdpReceiveResult receiveResult = await client.ServerEndPoint.ReceiveAsync();
+                Console.WriteLine("Message received");
+                Message newMessage = MessageToBytesConverter.BytesToMessage(receiveResult.Buffer);
+                client.MessagesUprocessed.Add(newMessage);
+
+            }
+        }
+        public static async Task SenderAsync(Client client)
+        {
+
+            while (client.Exit != true || client.MessagesUprocessed.Count > 0)
+            {
+                if (client.MessagesUprocessed.Count > 0)
+                {
+                    Message messageToProcess = client.MessagesUprocessed.Take();
+                    LogMessageReceived(client.ClientEndPoint, messageToProcess);
+
+                    await SendConfirmAsync(messageToProcess.Id, client.ServerEndPoint, client.ClientEndPoint);
+                    switch (messageToProcess.Type)
+                    {
+                        case MessageType.AUTH:
+                            {
+                                Console.WriteLine("CLIENT MESSAGE IS AUTH");
+                                if (client.State == ClientState.NONAUTH)
+                                {
+                                    Console.WriteLine("CLIENT IS NOT AUTHENTICATED");
+                                    if (client.Username == null)
+                                    {
+                                        client.Username = messageToProcess.Username;
+                                    }
+                                    client.Displayname = messageToProcess.DisplayName;
+
+                                    Message msg = new Message(client.messageId++, MessageType.REPLY);
+                                    msg.Result = true;
+                                    msg.Content = "Success! You are authenticated";
+                                    msg.RefId = messageToProcess.Id;
+                                    byte[] msgBytes = MessageToBytesConverter.MessageToBytes(msg);
+                                    bool confirmArrived = await SendAndWaitForConfirmAsync(client, msg);
+                                    if (confirmArrived == false)
+                                    {
+                                        // Just close the connection
+                                    }
+                                    else
+                                    {
+                                        // Send Message to all users in channel
+                                    }
+
+                                }
+                                else
         {
             Console.WriteLine("Hello, World!");
         }
     }
+
 }
