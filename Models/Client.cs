3b912dacd6c7be32bbe8193b35e9a3aa332946f3
xklima34 <xklima34@stud.fit.vutbr.cz>
2024-04-22 00:55:58 +0200

    EndPoints, Message collections added to Client model

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Collections.Concurrent;
 
 namespace IOTA_Chat_Server.Models
 {
@@ -11,14 +14,36 @@ namespace IOTA_Chat_Server.Models
         public string Username { get; set; }
         public string Displayname { get; set; }
         public string? CurrentChannelName { get; set; }
+        public IPEndPoint ClientEndPoint { get; set; }
+        public BlockingCollection<Message> ClientConfirms { get; set; }
+        public BlockingCollection<Message> MessagesUprocessed { get; set; }
+        public BlockingCollection<byte> BytesSent { get; set; }
+        public UdpClient ServerEndPoint { get; set; }
+        public UInt16 messageId { get; set; }
         public ClientState State { get; set; }
 
-    };
+        public bool Exit { get; set; }
+    
+
+        public Client(IPEndPoint clientEP, UdpClient serverEP)
+        { 
+            CurrentChannelName = "Default";
+            ClientEndPoint = clientEP;
+            ServerEndPoint = serverEP;
+            ClientConfirms = new BlockingCollection<Message>();
+            MessagesUprocessed = new BlockingCollection<Message>();
+            BytesSent = new BlockingCollection<byte>();
+            State = ClientState.NONAUTH;
+            messageId = 0;
+            Exit = false;
+        }
+    }
     
     public enum ClientState
     {
+    
         NONAUTH, 
         AUTH,
         BYE
     }
-}
+};
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
+﻿using System.Collections.Concurrent;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+using IOTA_Chat_Server.Models;
+using IOTA_Chat_Server.Exceptions;
+using IOTA_Chat_Server.Converters;
+using System.Reflection.Metadata;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 ﻿namespace IOTA_Chat_Server
 {
     internal class Program
